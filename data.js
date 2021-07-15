c88b93370c454db7acb7f1832a7fde4b4dc12af8
Attachment Studios <72270541+Attachment-Studios@users.noreply.github.com>
2021-07-15 18:50:54 +0530

    DATA

--- data.js
+++ data.js
@@ -1,2 +1,43 @@
 
+/*
+  Format:
+  [
+    "name",
+    "description",
+    "author",
+    "icon-link",
+    "page-link",
+    [
+      "tag1",
+      "tag2",
+      "tag3"
+    ]
+  ],
+  
+  Blank:
+  [
+    "",
+    "",
+    "",
+    "",
+    "",
+    []
+  ]
+*/
 
+data = [
+  [
+    "The Box",
+    "The Box is an easy simple arcade game about jumping.",
+    "Attachment Aditya, Attachment Studios",
+    "https://github.com/Attachment-Studios/the-box/blob/main/LOGO.png?raw=true",
+    "https://attachment-studios.github.io/the-box",
+    [
+      "Arcade",
+      "Endless",
+      "2D",
+      "Jumping",
+      "Platformer"
+    ]
+  ],
+]
